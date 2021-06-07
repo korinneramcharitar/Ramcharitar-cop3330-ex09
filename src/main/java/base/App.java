cc48871e9876b99a5d1565955c0af8ef873a67f2
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 21:01:22 -0400

    Update App.java

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,3 +1,8 @@
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Korinne Ramcharitar
+ */
 package base;
 
 //Sometimes you have to round up to the next number rather than follow standard rounding rules.
